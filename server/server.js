36b50e756b9edc3c0635a43429ee0a539492872e
Harsh Shah <skharsh.shah@gmail.com>
2025-08-21 18:42:32 +0530

    server.js updates

--- server/server.js
+++ server/server.js
@@ -59,12 +59,9 @@ app.use("/api/messages", messageRouter);
 
 await connectDB();
 
-// if(process.env.NODE_ENV !== "production"){
-//     const PORT = process.env.PORT || 5000;
-//     server.listen(PORT, ()=> console.log("server is running on port" + PORT));
-// }
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, ()=> console.log("server is running on port" + PORT));
+}
 
-// export default server;
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("server is running on port" + PORT));
+export default server;
